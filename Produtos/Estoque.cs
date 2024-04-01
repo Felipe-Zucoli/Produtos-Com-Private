b70b3c9d91cd387164c587df1f7cc910873f9b70
Felipe-Zucoli <zucolifelipe@gmail.com>
2024-04-01 14:37:04 -0300

    Adicionar arquivos de projeto.

---
+++ Produtos/Estoque.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produtos
+{
+     class Estoque
+    {
+       private string _nameProd;
+       private double _precoProd;
+       private int _quantProd;
+
+        public double _valorTotal()
+        {
+           return _precoProd* _quantProd;
+        }
+        public void AdicionarProduto (int quantidade)
+        {
+            _quantProd = _quantProd + quantidade;
+        }
+        public void RetirarProduto (int quantidade)
+        {
+            _quantProd = _quantProd - quantidade;
+        }
+
+        public Estoque (string nome, double preco, int quantidade)
+        {
+            _nameProd = nome;
+            _precoProd = preco;
+            _quantProd = quantidade;
+        }
+        public string GetName ()
+        {
+            return _nameProd;
+        }
+        
+
+        public double GetPrecoProd()
+        {
+            return _precoProd;
+        }
+
+        public int GetQuantProd() { 
+            return _quantProd;
+        }
+
+        public void SetName (string nome)
+        {
+            if (nome != null && nome.Length > 2) { 
+            _nameProd=nome;
+            } else
+            {
+                Console.WriteLine("Erro, nome inválido!");
+            }
+        }
+
+        
+
+        public override string ToString()
+        {
+            return _nameProd + ",Preço $" + _precoProd + ", Quantidade " + _quantProd +",Valor total $" + _valorTotal();
+        }
+    }
+}
---
+++ Produtos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Produtos;
+using System.Globalization;
+
+namespace Course { 
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Estoque estoque = new Estoque ("Tv", 500.00, 10);
+            estoque.SetName("Fe");
+            Console.WriteLine(estoque.GetName());
+            Console.WriteLine(estoque.GetPrecoProd()); 
+            Console.WriteLine (estoque.GetQuantProd());
+            
+    }
+}
+}
